5b5bb853d0af72a594f9da111bef133277039693
Bruno Produit <bruno.produit@gmail.com>
2018-10-07 03:30:10 +0300

    added the ckanextentions submodules

--- ckanext-converter/setup.py
+++ ckanext-converter/setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/timotoots/ckanext-converter',
+    url='https://github.com/timotoots/laastutabloo',
 
     # Author details
     author='''Laastutabloo''',
