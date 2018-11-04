a20cd5aeb67a914698e1a36d1211957b5f62952e
Bruno Produit <bruno.produit@gmail.com>
2018-11-04 12:37:45 +0200

    Converter test

--- ckanext-converter/ckanext/converter/plugin.py
+++ ckanext-converter/ckanext/converter/plugin.py
@@ -1,5 +1,7 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ujson
+import urllib2
 
 class ConverterPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -10,3 +12,8 @@ class ConverterPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'converter')
+    
+    def get():
+        request = urllib2.Request('http:/data.laastutabloo.ee/api/action/user_list')
+        response_dict = ujson.loads(urllib2.urlopen(request, '{}').read())
+        print response_dict
