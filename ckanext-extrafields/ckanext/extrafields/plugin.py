b680fe83f37fdb98da367c3c5f6ba2df6617846b
Sander Mikelsaar <sandermi@ut.ee>
2018-11-10 16:19:32 +0200

    Adds update_interval to resources, removed custom_text_field from datasets

--- ckanext-extrafields/ckanext/extrafields/plugin.py
+++ ckanext-extrafields/ckanext/extrafields/plugin.py
@@ -7,24 +7,16 @@ class ExtrafieldsPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
 	p.implements(p.IConfigurer)
 
 	def _modify_package_schema(self, schema):
-		schema.update({
-			'custom_text': [tk.get_validator('ignore_missing'),
-			tk.get_converter('convert_to_extras')]
-		})
 		schema['resources'].update({
-			'custom_resource_text' : [ tk.get_validator('ignore_missing'),
+			'update_interval' : [ tk.get_validator('ignore_missing'),
 			tk.get_converter('convert_to_extras')]
 		})
 		return schema
 
 	def show_package_schema(self):
 		schema = super(ExtrafieldsPlugin, self).show_package_schema()
-		schema.update({
-			'custom_text': [tk.get_converter('convert_from_extras'),
-			tk.get_converter('convert_to_extras')]
-		})
 		schema['resources'].update({
-			'custom_resource_text' : [ tk.get_validator('ignore_missing'),
+			'update_interval' : [ tk.get_validator('ignore_missing'),
 			tk.get_converter('convert_to_extras')]
 		})
 		return schema
