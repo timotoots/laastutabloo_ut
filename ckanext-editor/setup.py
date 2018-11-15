23d6c8d49caa428bd767d0fc4a9804de88efe513
Bruno Produit <bruno.produit@gmail.com>
2018-11-15 17:12:38 +0200

    editor v1

--- ckanext-editor/setup.py
+++ ckanext-editor/setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/brunoproduit/ckanext-editor',
+    url='https://github.com/timotoots/laastutabloo',
 
     # Author details
     author='''Bruno Produit''',
