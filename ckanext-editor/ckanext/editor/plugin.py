2097920fdc589d2fd38cf749488739bcbdc698fb
Bruno Produit <bruno.produit@gmail.com>
2018-12-14 09:37:38 +0200

    Data columns

--- ckanext-editor/ckanext/editor/plugin.py
+++ ckanext-editor/ckanext/editor/plugin.py
@@ -1,34 +1,37 @@
 # encoding: utf-8
 
 from logging import getLogger
-from flask import Markup
 from ckan.common import json, config
 import ckan.plugins as p
 import ckan.plugins.toolkit as toolkit
-import xmltodict, json
 from lxml import etree
-from collections import Counter
 from ckan.common import config
-import re
-FILESTORAGE_PATH = config.get('ckan.storage_path') 
+import pandas
 
+FILESTORAGE_PATH = config.get('ckan.storage_path')
 log = getLogger(__name__)
 ignore_empty = p.toolkit.get_validator('ignore_empty')
 natural_number_validator = p.toolkit.get_validator('natural_number_validator')
 Invalid = p.toolkit.Invalid
 
-def walk(root, level=0):
-    res = '<li><input type="ccheckbox" class="form-check-input" id=' + root.tag + str(level) +'>' + '<label class="form-check-label" for=' + root.tag + str(level) + '>  ' + root.tag + ' </label><ul class="nested">'
-    for i in list(root):
-        if list(i) != []:
-            res += walk(i, level + 1)
-        else:
-          res += '<li><input type="checkbox" class="form-check-input" id=' + root.tag + str(level) + i.tag + '>   ' + '<label class="form-check-label" for=' + root.tag + str(level) + i.tag + '>  ' + i.tag + '</label></li>'
+def xml(file_path):
+    tree = ET.parse(file_path)
 
-    res += "</li></ul>"
-     
-    return res
+    data = []
+    tmp = {}
+    for i in tree.iterfind('./*'):
+        for j in i.iterfind('*'):
+            tmp[j.tag] = j.text
+        data.append(tmp)
+        tmp = {}
 
+    return pandas.DataFrame(data)
+
+def json(file_path):
+    return pandas.read_json(file_path)
+
+def csv(file_path):
+    return pandas.read_csv(file_path)
 
 class EditorPlugin(p.SingletonPlugin):
     '''
@@ -36,8 +39,8 @@ class EditorPlugin(p.SingletonPlugin):
     '''
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.IResourceView, inherit=True)
-    
-   
+
+
     def info(self):
         return {'name': 'editor',
                 'title': 'Editor',
@@ -46,7 +49,7 @@ class EditorPlugin(p.SingletonPlugin):
                 'requires_datastore': False,
                 'default_title': p.toolkit._('Editor'),
                 }
-    
+
     def update_config(self, config):
         '''
         Set up the resource library, public directory and
@@ -55,7 +58,7 @@ class EditorPlugin(p.SingletonPlugin):
         toolkit.add_public_directory(config, 'public')
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_resource('fanstatic', 'editor')
-        
+
     def can_view(self, data_dict):
         resource = data_dict['resource']
         resource_format = resource.get('format', None)
@@ -63,20 +66,31 @@ class EditorPlugin(p.SingletonPlugin):
             return resource_format.lower() in ['xml', 'csv', 'json']
         else:
             return False
-   
+
     def view_template(self, context, data_dict):
         return 'base.html'
-                       
+
     def setup_template_variables(self, context, data_dict):
         resource = data_dict['resource']
         filetype = resource["format"]
         file_id = resource['id']
         file_path = FILESTORAGE_PATH + "/resources/" + file_id[:3] + "/" + file_id[3:6] + "/" + file_id[6:]
         # Pass dataset to js
-        data = open(file_path, 'r').read()       
-        
+        data = open(file_path, 'r').read()
+
+        if file_type=='XML':
+            data = xml(file_path)
+        elif file_type=='JSON':
+            data = json(file_path)
+        elif file_type=='CSV':
+            data = csv(file_path)
+        else:
+    	      print "Not a valid file type"
+
+        data = data.to_json(encoding='utf-8')
+        print data
+
         # Fetch current user's api_key and pass it to js
         api_key = toolkit.get_action('user_show')(context, {'id': toolkit.c.userobj.id})['apikey']
         dataset_id = toolkit.c.id
         return {'data': data, 'api_key': api_key, 'dataset_id': dataset_id}
-
